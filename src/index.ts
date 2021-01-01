95206fb90635318e3eb53ac4528efa580dabb1e0
Denis <k1selevde@yandex.ru>
2021-01-01 22:42:04 +0500

    server tsc changes

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,17 @@
 import express from "express";
-const cors = require('cors')
-const path = require('path')
+import cors from "cors"
+import path from "path"
 import bodyParser from "body-parser";
 
 
-const PORT = process.env.PORT || 5000;
+const PORT : string|number = process.env.PORT || 5000;
 
 const app = express();
 
-
-app
-    .use('/', express.static(path.resolve(__dirname,  'client', 'build')))
-    .use(cors())
-    .use(bodyParser.json());
-
-app.get('*', (req: express.Request, res: express.Response) => {
+app.use("*",(req, res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+});
 
 
-const server = app.listen(PORT, function () {
-    console.log('Server has been started! YeYs')
-})
 
-
+app.listen(PORT,() => console.log(`hosting @${PORT}`));
