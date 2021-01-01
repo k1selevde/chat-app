e9a867e77dfd3769a692a5d7a1487735c3a7ee31
Denis <k1selevde@yandex.ru>
2021-01-01 23:42:47 +0500

    1

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors"
 import path from "path"
-import bodyParser from "body-parser";
+
 
 
 const PORT : string|number = process.env.PORT || 5000;
 
 const app = express();
 
-app.use("*",(req, res) =>{
+
+app
+    .use('/', express.static(path.resolve(__dirname,  'client', 'build')))
+    .use(cors())
+
+
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-});
-
-
+})
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
