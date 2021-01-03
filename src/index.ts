cfdeabd1ddce31bf9eaf63228861a98ce75ca7dd
Denis <k1selevde@yandex.ru>
2021-01-03 13:30:26 +0500

    wrap production base route

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import express from 'express';
+import express, { Application, Response, Request, NextFunction} from 'express';
 import path from 'path'
 
-const app = express()
+const app: Application = express()
 
 const port : string|number= process.env.PORT || 5000;
 
-app
-    .use('/', express.static(path.resolve(__dirname,  '../client', 'build')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname,  '../client', 'build')))
+
+    app.get("*",(req: Request, res: Response) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    })
+}
 
 
-app.use("*",(req: express.Request, res: express.Response) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-})
+
+
 
 //create a server object:
 app.listen(port,() => console.log(`hosting @${port}`));
